2e7ea418572ffc03d0c32e325b4c84eac4482ec9
frruzix <61508170+dontbitemepls@users.noreply.github.com>
2025-10-02 12:07:38 +0300

    Update Lab2 C++.cpp

--- Lab2 C++/Lab2 C++.cpp
+++ Lab2 C++/Lab2 C++.cpp
@@ -49,7 +49,47 @@ int main()
             cout << "Введен некорректный знак" << endl;
     }
 
+    	// ЧАCТЬ 2.
+
+	int num;
+	while (true) {
+		cout << "Введите целое положительное число: " << endl;
+		cin >> num;
+		if (num > 0) {
+			break;
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i <= num; i++) {
+		ans += i;
+	}
+	cout << ans << endl;
+
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int length = sizeof(arr) / sizeof(arr[0]);
+	for (int a : arr) {
+		cout << a << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < length; i++) {
+		if (i % 2 == 0) {
+			cout << arr[i] << " ";
+		}
+	}
+	cout << endl;
+
+	ans = 0;
+	for (int i = 0; i < length; i++) {
+		if (i % 2 != 0) {
+			ans += arr[i];
+		}
+	}
+	cout << ans << endl;
+}
 
 }
+
 
 
